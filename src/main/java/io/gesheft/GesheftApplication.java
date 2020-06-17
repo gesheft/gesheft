b2b5cf2a868644c96e93ffac7cad81c66cf6d816
Rostyslav Rava <rostyslav.rava@gmail.com>
2020-06-17 23:59:42 +0300

    Initial commit

---
+++ src/main/java/io/gesheft/GesheftApplication.java
@@ -0,0 +1,13 @@
+package io.gesheft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesheftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesheftApplication.class, args);
+	}
+
+}
